fbee8af7b25938a970cb3e7b2dc4d57eb7a8e38a
tanjima12 <tanjimaakter005@gmail.com>
2023-10-20 23:35:31 +0600

    get delete

--- index.js
+++ index.js
@@ -110,6 +110,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    //delete
+    app.delete("/CartCollect/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
